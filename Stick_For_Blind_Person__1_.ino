59a193cd50e5232937cf294c0f1c35e458b791ed
Tasnim Haque <tasnimhaque2001abc@gmail.com>
2026-08-21 20:03:02 +0600

    Add files via upload

---
+++ Stick_For_Blind_Person__1_.ino
@@ -0,0 +1,48 @@
+const int trigPin = 9;
+const int echoPin = 10;
+const int buzzer = 11;
+const int ledPin = 13;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+ 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  duration = pulseIn(echoPin, HIGH, 30000); 
+
+  distance = duration * 0.034 / 2;
+
+
+  if (distance == 0) {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(ledPin, LOW);
+    return;
+  }
+
+  if (distance <= 40) {
+    digitalWrite(buzzer, HIGH);
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(ledPin, LOW);
+  }
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
